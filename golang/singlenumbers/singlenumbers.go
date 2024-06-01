fab3881298380cd396be77171c288f1095368e93
Matteo Veraldi <mattveraldi@gmail.com>
2024-06-01 18:23:44 +0200

    refactor folders

--- golang/singlenumbers/singlenumbers.go
+++ golang/singlenumbers/singlenumbers.go
@@ -1,4 +1,4 @@
-package main
+package singlenumbers
 
 // Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
 // You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
--- golang/singlenumbers/singlenumbers_test.go
+++ golang/singlenumbers/singlenumbers_test.go
@@ -1,30 +1,30 @@
-package main
+package singlenumbers
 
 import (
 	"reflect"
 	"testing"
 )
 
-func test_suite(t *testing.T) {
+func TestSuite(t *testing.T) {
 	example_one_nums := []int{1, 2, 1, 3, 2, 5}
 	example_one_expected := []int{3, 5}
 	example_two_nums := []int{-1, 0}
 	example_two_expected := []int{-1, 0}
 	example_three_nums := []int{0, 1}
-	example_three_expected := []int{1, 0}
+	example_three_expected := []int{0, 1}
 
 	example_one_result := SingleNumber(example_one_nums)
 	if !reflect.DeepEqual(example_one_result, example_one_expected) {
-		t.Failed()
+		t.Fatalf("Failed test one")
 	}
 
 	example_two_result := SingleNumber(example_two_nums)
 	if !reflect.DeepEqual(example_two_result, example_two_expected) {
-		t.Failed()
+		t.Fatalf("Failed test two")
 	}
 
 	example_three_result := SingleNumber(example_three_nums)
 	if !reflect.DeepEqual(example_three_result, example_three_expected) {
-		t.Failed()
+		t.Fatalf("Failed test three")
 	}
 }
